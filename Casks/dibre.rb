8c9c2c1020937e6fce205955c8356985ec95c145
Péricles Pires <periclespires@protonmail.com>
2026-06-11 14:47:28 -0300

    Fix download URL for Dibre cask
    Updated the download URL for the Dibre cask.

--- Casks/dibre.rb
+++ Casks/dibre.rb
@@ -1,14 +1,9 @@
 cask "dibre" do
   version "1.0.0"
 
-  # Este é o hash de integridade do arquivo.
-  # Quando a pipeline rodar e o .dmg for gerado na sua release, você deve calcular o hash dele.
-  # Exemplo no terminal: shasum -a 256 caminho/para/Dibre_1.0.0_aarch64.dmg
   sha256 "d4fcf88f7c95d2d6bf107f1be325ac85efc03b4c6cbb804229e2b0bc0161c6bd"
 
-  # A URL de onde o brew vai baixar o instalador
-  # Como a Action que criamos roda no macos-latest (que hoje em dia é Apple Silicon), o Tauri gera o arquivo com o sufixo _aarch64.dmg
-  url "https://github.com/bitpickle/dibre/releases/download/v#{version}/Dibre_#{version}_aarch64.dmg"
+  url "https://github.com/bitpickle/homebrew-dibre/releases/download/v#{version}/Dibre_#{version}_aarch64.dmg"
 
   name "Dibre"
   desc "Modern and dynamic database client"
@@ -16,8 +11,6 @@ cask "dibre" do
 
   app "Dibre.app"
 
-  # Opcional, mas muito bom ter:
-  # Limpa o "lixo" quando alguém rodar 'brew uninstall --zap dibre'
   zap trash: [
     "~/Library/Application Support/dev.bitpickle.dibre",
     "~/Library/Preferences/dev.bitpickle.dibre.plist",
